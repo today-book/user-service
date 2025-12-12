837015820a5417e1692109e0d06a186c0204142b
hyeon48165 <hyeon48615@gmail.com>
2025-12-12 14:50:51 +0900

    [#1] Fix: 빌드 테스트 워크플로우 오류 수정

--- src/test/java/org/todaybook/userservice/domain/UserBookTests.java
+++ src/test/java/org/todaybook/userservice/domain/UserBookTests.java
@@ -6,9 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.todaybook.userservice.BookFixture;
 import org.todaybook.userservice.domain.dto.Book;
 
+@ActiveProfiles("test")
 class UserBookTests {
 
   @Test
