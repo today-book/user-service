3e50d774f48b96e04f154c9aa57edf47957681e8
hyeon48165 <hyeon48615@gmail.com>
2025-12-14 13:52:41 +0900

    [#2] Test: 내 책장 저장/삭제 도메인 서비스 단위 테스트

--- src/test/java/org/todaybook/userservice/BookFixture.java
+++ src/test/java/org/todaybook/userservice/BookFixture.java
@@ -3,16 +3,9 @@ package org.todaybook.userservice;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
-import org.todaybook.userservice.userbook.domain.BookId;
-import org.todaybook.userservice.userbook.domain.UserBook;
-import org.todaybook.userservice.userbook.domain.UserId;
 import org.todaybook.userservice.userbook.domain.dto.Book;
 
 public class BookFixture {
-
-  public static UserBook userBook(Long id, BookId bookId) {
-    return UserBook.create(UserId.of(id), book(bookId.toUUID()));
-  }
 
   public static Book book(UUID id) {
     return Book.builder()
