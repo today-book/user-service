8647ed2cde225ba6a2b8efc683afe9ba48133c41
hyeon48165 <hyeon48615@gmail.com>
2025-12-19 16:12:29 +0900

    [#35] Refactor: 예외 처리 공통화
    - 예외 메세지 통일
    - ErrorIdentifier 사용

--- src/main/java/org/todaybook/userservice/share/domain/exception/SharedBookNotFoundException.java
+++ src/main/java/org/todaybook/userservice/share/domain/exception/SharedBookNotFoundException.java
@@ -1,10 +1,12 @@
 package org.todaybook.userservice.share.domain.exception;
 
+import java.util.UUID;
 import org.todaybook.commoncore.error.AbstractServiceException;
+import org.todaybook.userservice.global.exception.ErrorIdentifier;
 
 public class SharedBookNotFoundException extends AbstractServiceException {
 
-  public SharedBookNotFoundException(Object... args) {
-    super(ShareErrorCode.BOOK_NOT_FOUND, args);
+  public SharedBookNotFoundException(UUID token) {
+    super(ShareErrorCode.BOOK_NOT_FOUND, ErrorIdentifier.of("token", token));
   }
 }
--- src/main/java/org/todaybook/userservice/user/domain/exception/UserNotFoundException.java
+++ src/main/java/org/todaybook/userservice/user/domain/exception/UserNotFoundException.java
@@ -1,10 +1,17 @@
 package org.todaybook.userservice.user.domain.exception;
 
 import org.todaybook.commoncore.error.AbstractServiceException;
+import org.todaybook.userservice.global.exception.ErrorIdentifier;
+import org.todaybook.userservice.user.domain.KakaoId;
+import org.todaybook.userservice.user.domain.UserId;
 
 public class UserNotFoundException extends AbstractServiceException {
 
-  public UserNotFoundException(Object... args) {
-    super(UserErrorCode.NOT_FOUND, args);
+  public UserNotFoundException(UserId userId) {
+    super(UserErrorCode.NOT_FOUND, ErrorIdentifier.of("userId", userId));
+  }
+
+  public UserNotFoundException(KakaoId kakaoId) {
+    super(UserErrorCode.NOT_FOUND, ErrorIdentifier.of("kakaoId", kakaoId));
   }
 }
